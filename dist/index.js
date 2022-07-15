815208069b77ed704b03548c491fc295765b7e16
Bruce Song <recall4056@gmail.com>
2022-07-15 08:08:30 +0800

    Add debug file.

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,7 @@
 "use strict";
 const HELLO_TYPESCRIPT = "Hello TypeScript!";
+let age = 22;
+if (age < 50) {
+    age += 10;
+}
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-const HELLO_TYPESCRIPT: String = "Hello TypeScript!";
+const HELLO_TYPESCRIPT: string = "Hello TypeScript!";
+let age: number = 22;
+if (age < 50) {
+  age += 10;
+}
