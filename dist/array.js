e0d78727239b790428a2138129991da8a4567ce2
Bruce Song <recall4056@gmail.com>
2022-07-16 01:13:43 +0800

    Update.

--- dist/array.js
+++ dist/array.js
@@ -2,4 +2,5 @@
 let numbersInJS = [1, 2, "3"];
 let numbersInTS = [1, 2, 3];
 let numbersOrStrings = [1, 2, "3"];
+numbersInJS.forEach((x) => console.log(x));
 //# sourceMappingURL=array.js.map
--- src/array.ts
+++ src/array.ts
@@ -1,3 +1,5 @@
 let numbersInJS = [1, 2, "3"]; // The element which index is 2 is not safety.
 let numbersInTS: number[] = [1, 2, 3];
 let numbersOrStrings: (number | string)[] = [1, 2, "3"];
+
+numbersInJS.forEach((x) => console.log(x));
