70183e08ef10561775a1931b76a9d36d12ae000f
Bruce Song <recall4056@gmail.com>
2022-07-16 00:21:13 +0800

    Update tsconfig.

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,18 @@ if (age < 50) {
   age += 10;
 }
 console.log(age);
+
+let sales: number = 123456789;
+let course: string = "TypeScript";
+let isPublished: boolean = true;
+
+// If a variable doesn't initialize with type declaration, it will be assigned to type 'any'.
+let level;
+level = 1;
+level = "1";
+
+function render(document: string) {
+  console.log(document);
+}
+
+level = render(level);
